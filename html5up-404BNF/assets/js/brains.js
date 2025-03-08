5b0b1c07b40655cfffd28b3ef22f6750ad8c004e
Mia Yancey <meyance@clemson.edu>
2025-03-08 13:14:21 -0500

    pulling api calls back in

--- html5up-404BNF/assets/js/brains.js
+++ html5up-404BNF/assets/js/brains.js
@@ -2,7 +2,7 @@
 let riddleMap = new Map(); // Global storage for POIs and their riddles
 
 async function getPointsOfInterest(lat, lng) {
-    //let apiKey = MapAPIPhrase(); // TODO: Replace with API Key
+    let apiKey = MapAPIPhrase(); // TODO: Replace with API Key
     const radius = 1609; // 1 mile in meters
     const type = 'point_of_interest';
     const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=${radius}&type=${type}&key=${apiKey}`;
@@ -27,7 +27,7 @@ async function getPointsOfInterest(lat, lng) {
 }
 
 async function getRiddlesForPOIs(pois) {
-    //let apiKey = OpenaiAPIPhrase(); // TODO: Replace with API Key
+    let apiKey = OpenaiAPIPhrase(); // TODO: Replace with API Key
     const prompt = pois.map(poi => `Create a riddle about a place called "${poi.name}", which is located at "${poi.address}".`).join("\n");
 
     const body = {
